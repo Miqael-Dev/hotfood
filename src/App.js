28919ae516c482f1d36849244c6e93ce0f9b5687
Miqael <miqael.dev@gmail.com>
2023-04-28 15:40:37 +0000

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react"
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+/* Local imports */
 import Home from './components/Home'
 
 
-
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>
+    }
+  ])
   return (
     <div>
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
