fa6b3d2881a04298269d16909a04d2b98e2dbd59
Miqael <miqael.dev@gmail.com>
2023-05-08 22:40:59 +0000

    Add Menu Section -install Material-UI -Create images folder in public folder -Link Hosted JSON data of meals to menu cards -limit menu section to 20 cards -Fix openCloseBanner bug to dateHour < 21 -Use MUI for star rating and categories in menu section -Relocate favicon to Images folder in public folder -Add multiple background images for design testing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react"
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 /* Local imports */
-import Home from './components/Home'
+import Home, { mealsloader } from './components/Home'
 
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>
+      element: <Home/>,
+      loader: mealsloader
     }
   ])
   return (
